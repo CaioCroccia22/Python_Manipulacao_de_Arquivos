515fda525e1d255da6f3e6e1470650ec48b3d444
caiocroccia22 <caiocroccia22@gmail.com>
2024-03-08 18:40:48 -0300

    Manipulando arquivo CSV

---
+++ leituracsv.py
@@ -0,0 +1,7 @@
+with open("dados/courses.csv", "r", encoding="utf-8") as file:
+        for line in file:
+                # valores em forma de listas
+                # row = line.rstrip().split(",")
+                # print(f"{row[0]} -{row[1]}")
+                language, category = line.rstrip().split(",")
+                print(f"{language} - {category}")
